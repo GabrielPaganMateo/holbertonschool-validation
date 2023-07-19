03dc263a63330124fe53c94bb0aa4e18ce620815
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-19 06:34:50 -0400

    again

--- module3_task1/setup.sh
+++ module3_task1/setup.sh
@@ -8,5 +8,5 @@ apt-get install -y make wget
 # Install the required version of Hugo (0.84.0)
 HUGO_VERSION=0.84.0
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.deb
-dpkg -i hugo_${HUGO_VERSION}_Linux-64bit.deb
+sudo dpkg -i hugo_${HUGO_VERSION}_Linux-64bit.deb
 rm hugo_${HUGO_VERSION}_Linux-64bit.deb
