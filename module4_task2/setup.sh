85521f773c6a85ab208317a2ed9ba078d4cada16
GabrielPaganMateo <113718820+GabrielPaganMateo@users.noreply.github.com>
2023-07-25 11:26:11 -0400

    is it fixed ?

--- module4_task2/setup.sh
+++ module4_task2/setup.sh
@@ -3,18 +3,10 @@
 apt-get update
 
 # Install required packages
-apt-get install -y make wget curl
-
-# Install Node.js and npm
-curl -sL https://deb.nodesource.com/setup_14.x | bash -
-apt-get install -y nodejs
-
-# Verify Node.js and npm installation
-node -v
-npm -v
+apt-get install -y make wget
 
 # Install the required version of Hugo (0.84.0)
 HUGO_VERSION=0.84.0
 wget https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.deb
-dpkg -i hugo_${HUGO_VERSION}_Linux-64bit.deb
+sudo dpkg -i hugo_${HUGO_VERSION}_Linux-64bit.deb
 rm hugo_${HUGO_VERSION}_Linux-64bit.deb
